ad5c104404701059db83b7b258d2417ba7e4733e
Laveen Kumar <laveen.kumar@updaptcsr.com>
2025-03-04 23:38:06 +0530

    feat:added code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Keyboard from "./keyboard";
 
 function App() {
+  // Safely parse user ID
+  const searchParams = new URLSearchParams(window.location.search);
+  const userIdParam = searchParams.get("user");
+  const userId = userIdParam ? parseInt(userIdParam, 10) : 2;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Keyboard userId={userId} />
     </div>
   );
 }
